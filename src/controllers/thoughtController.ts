be94b9f1022327b0f31938d2ad07847902f874b7
victoriamata <victoriamataxo@gmail.com>
2024-11-23 19:29:09 -0500

    Added thought controllers and routes

---
+++ src/controllers/thoughtController.ts
@@ -0,0 +1,131 @@
+import { Thought, User } from '../models/index.js';
+import { Request, Response } from 'express';
+
+export const getThoughts = async (_req: Request, res: Response) => {
+  try {
+    const thoughts = await Thought.find();
+    res.json(thoughts);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+}
+
+export const getSingleThought = async (req: Request, res: Response) => {
+  try {
+    const thought = await Thought.findOne({ _id: req.params.thoughtId })
+
+    if (!thought) {
+      return res.status(404).json({ message: 'No thought with that ID' });
+    }
+
+    res.json(thought);
+    return;
+  } catch (err) {
+    res.status(500).json(err);
+  }
+
+  return;
+}
+
+export const createThought = async (req: Request, res: Response) => {
+  try {
+    const user = await User.findOne({ username: req.body.username });
+    const thought = await Thought.create(req.body);
+
+    if (!user) {
+      return res.status(404).json({ message: 'User not found by username' });
+    }
+
+    await User.findOneAndUpdate(
+      { username: req.body.username },
+      { $addToSet: { thoughts: thought._id } },
+      { new: true }
+    );
+
+    res.json('Thought successfully created');
+    return;
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+
+  return;
+}
+
+export const updateThought = async (req: Request, res: Response) => {
+  try {
+    const thought = await Thought.findOneAndUpdate(
+      { _id: req.params.thoughtId },
+      { $set: req.body },
+      { runValidators: true, new: true }
+    );
+
+    if (!thought) {
+      return res.status(404).json({ message: 'Thought not found' });
+    }
+
+    res.json(thought);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+}
+
+export const deleteThought = async (req: Request, res: Response) => {
+  try {
+    const thought = await Thought.findOneAndDelete({ _id: req.params.thoughtId });
+
+    if (!thought) {
+      return res.status(404).json({ message: 'Thought not found by ID' });
+    }
+
+    const user = await User.findOneAndUpdate(
+      { thoughts: req.params.thoughtId },
+      { $pull: { thoughts: req.params.thoughtId } },
+      { new: true }
+    );
+
+    if (!user) {
+      return res.status(404).json({ message: 'User not found by ID' });
+    }
+
+    res.json({ message: 'Thought successfully deleted' });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+}
+
+export const addThoughtReaction = async (req: Request, res: Response) => {
+  try {
+    const thought = await Thought.findOneAndUpdate(
+      { _id: req.params.thoughtId },
+      { $addToSet: { reactions: req.body } },
+      { runValidators: true, new: true }
+    );
+
+    if (!thought) {
+      return res.status(404).json({ message: 'Thought not found' });
+    }
+
+    res.json(thought);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+};
+
+export const removeThoughtReaction = async (req: Request, res: Response) => {
+  try {
+    const thought = await Thought.findOneAndUpdate(
+      { _id: req.params.thoughtsId }, 
+      { $pull: { reactions: { reactionId: req.params.reactionId } } },
+        { runValidators: true, new: true }
+    );
+
+    if (!thought) {
+      return res.status(404).json({ message: 'Thought not found' });
+    }
+
+    res.json(thought);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+};
---
+++ src/routes/api/thoughtRoutes.ts
@@ -0,0 +1,24 @@
+import { Router } from 'express';
+const router = Router();
+import { getThoughts, getSingleThought, createThought, updateThought, deleteThought, addThoughtReaction, removeThoughtReaction } from '../../controllers/thoughtController.js';
+
+router.route('/')
+.get(getThoughts)
+.post(createThought);
+
+// /api/thoughts/:thoughtId
+router
+  .route('/:thoughtId')
+  .post(createThought)
+  .get(getSingleThought)
+  .put(updateThought)
+  .delete(deleteThought);
+
+// /api/thoughts/:thoughtId/reactions
+router.route('/:thoughtId/reactions').post(addThoughtReaction);
+
+// /api/thoughts/:thoughtId/reactions/:reactionId
+router.route('/:thoughtId/reactions/:reactionId')
+.delete(removeThoughtReaction);
+
+export default router;
